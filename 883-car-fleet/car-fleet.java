0601301ef6f1f6db28a62f16ad01977bdc78c78d
Lilesh <lileshpatil38@gmail.com>
2026-09-23 15:49:34 +0530

    Time: 141 ms (5.86%) | Memory: 121.7 MB (5.02%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,16 +1,15 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-
-        TreeMap<Integer, Double> mp = new TreeMap<>(Collections.reverseOrder());
+        TreeMap<Integer, Double> tm = new TreeMap<>(Collections.reverseOrder());
         for (int i = 0; i < position.length; i++) {
-            double t = (double)(target - position[i]) / speed[i];
-            mp.put(position[i], t);
+            double time = (double) (target - position[i]) / speed[i];
+            tm.put(position[i], time);
         }
-        double leadTime = mp.get(mp.firstKey());
-        int count = 1;
-        for (double x : mp.values()) {
-            if (x > leadTime) {
-                leadTime = x;
+        int count = 0;
+        double mt = -1;
+        for (double crr : tm.values()) {
+            if (crr > mt) {
+                mt = crr;
                 count++;
             }
         }
